1930e63ef836c930959563871b9f35e88ab171c2
PavelPodymov <poll.ppbpower@gmail.com>
2022-11-09 14:12:52 +0300

    fix execption 'Null'

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ List<string> newArray = new List<string>();
 
 for(int i = 0; i < array.Length; i++)
 {
-    if (array[i].Length <= 3)
+    if (array[i].Length <= 3 && array[i] != "")
         newArray.Add(array[i]);
 }
 
