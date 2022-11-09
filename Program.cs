061365f08b898455e85454e9942375877a41e0da
PavelPodymov <poll.ppbpower@gmail.com>
2022-11-09 13:48:04 +0300

    Done programm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿
-Console.Clear();
+﻿Console.Clear();
+
+var str = Console.ReadLine();
+string[] array = str.Split(" ");
+List<string> newArray = new List<string>();
+
+for(int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+        newArray.Add(array[i]);
+}
+
+Console.WriteLine($"[{String.Join(", ", newArray)}]");
